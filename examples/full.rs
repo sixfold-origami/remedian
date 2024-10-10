3191c052f777547e71c33ce84f4a39b76a5cebc4
sixfold <rosepeck1997@gmail.com>
2024-10-09 21:07:16 -0400

    Add examples

---
+++ examples/full.rs
@@ -0,0 +1,39 @@
+//! A more fully-fledged example, showcasing custom confgiuration and other methods on [`RemedianBlock`]
+
+//! A basic example showing minimal usage
+//!
+//! We construct a [`RemedianBlock`], fill it with data, and then read out the approximate median
+
+use remedian::RemedianBlock;
+
+/// Some sample data to calculate the median for
+///
+/// In practice, this will probably be a much larger stream
+/// Note that the exact median is 44.5
+const DATA: [f32; 15] = [
+    18.6, 83.1, 21.5, 21.4, 63.4, 64.1, 4.6, 92.7, 31.1, 94.8, 2.4, 44.5, 70.0, 17.1, 61.0,
+];
+
+fn main() {
+    // Construct an empty remedian block, with a custom base and exponent
+    // These values will result in a block which uses 20 f32's of space, but can handle up to 625 sample points
+    let mut remedian = RemedianBlock::new(5, 4);
+
+    // Read data points from our data source, and fold them into the remedian
+    for data_point in DATA {
+        remedian.add_sample_point(data_point);
+
+        // As we go, we can check the current number of total points processed
+        println!("Processed {} data points so far", remedian.count());
+        // We can also check if we have run out of space yet
+        if remedian.locked() {
+            println!("Remedian is full: no further points can be processed");
+        } else {
+            println!("Remedian is not full");
+        }
+    }
+
+    // Once we've processed everything, we can get our answer out
+    let median = remedian.median();
+    println!("Approximated the median as: {median}");
+}
--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,5 +1,29 @@
+//! A basic example showing minimal usage
+//!
+//! We construct a [`RemedianBlock`], fill it with data, and then read out the approximate median
+
 use remedian::RemedianBlock;
 
+/// Some sample data to calculate the median for
+///
+/// In practice, this will probably be a much larger stream
+/// Note that the exact median is 44.5
+const DATA: [f32; 15] = [
+    18.6, 83.1, 21.5, 21.4, 63.4, 64.1, 4.6, 92.7, 31.1, 94.8, 2.4, 44.5, 70.0, 17.1, 61.0,
+];
+
 fn main() {
-    let _remedian = RemedianBlock::default();
+    // The default block is configured with a reasonable size
+    // It can account for roughly 25 billion sample points before running out of space
+    // But it stores at most 110 f32's at a time
+    let mut remedian = RemedianBlock::default();
+
+    // Read data points from our data source, and fold them into the remedian
+    for data_point in DATA {
+        remedian.add_sample_point(data_point);
+    }
+
+    // Get our (approximate) answer
+    let median = remedian.median();
+    println!("Approximated the median as: {median}");
 }
